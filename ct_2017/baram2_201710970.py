49a25a5063041c0356eed9569a64f667fd26ebcb
LimByungJin <dkhsjr@gmail.com>
2017-05-13 13:52:11 +0900

    Add files via upload

---
+++ ct_2017/baram2_201710970.py
@@ -0,0 +1,16 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def giyuk(size):
+    t1.fd(size)
+    t1.right(90)
+    t1.fd(size)
+i=0
+while i in range(0,8) :
+    i=i+1
+    giyuk(100)
+    t1.penup()
+    t1.home()
+    t1.pendown()
+    t1.right(45*i)
+wn.exitonclick()
