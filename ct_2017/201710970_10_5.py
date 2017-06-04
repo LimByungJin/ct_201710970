3a3db5c3747dc396d3d97ef70255b9725405800c
LimByungJin <dkhsjr@gmail.com>
2017-06-04 14:36:40 +0900

    Add files via upload

---
+++ ct_2017/201710970_10_5.py
@@ -0,0 +1,53 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+width=wn.window_width()
+x1=(0-(width-30)/3)
+x2=0
+x3=0+(width-30)/3
+def TriangleAT(x,y,size):
+	t1.setheading(0)
+	t1.penup()
+	t1.goto(x,y)
+	t1.pendown()
+	t1.fd(size)
+	t1.left(120)
+	t1.fd(size)
+	t1.left(120)
+	t1.fd(size)
+
+def PentagonAT(x,y,size):
+	t1.setheading(0)
+	t1.penup()
+	t1.goto(x,y)
+	t1.pendown()
+	t1.left(36)
+	t1.fd(size)
+	t1.right(72)
+	t1.fd(size)
+	t1.right(72)
+	t1.fd(size)
+	t1.right(72)
+	t1.fd(size)
+	t1.right(72)
+	t1.fd(size)
+
+def StarAT(x,y,size):
+	t1.setheading(0)
+	t1.penup()
+	t1.goto(x,y)
+	t1.pendown()
+	t1.fd(size)
+	t1.right(144)
+	t1.fd(size)
+	t1.right(144)
+	t1.fd(size)
+	t1.right(144)
+	t1.fd(size)
+	t1.right(144)
+	t1.fd(size)
+
+TriangleAT(x1,0,100)
+PentagonAT(x2,0,100)
+StarAT(x3,0,100)
+wn.exitonclick()
